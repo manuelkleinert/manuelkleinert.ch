cd733a955b95859f0bc619dd3f2d2d918d05ce6a
Manuel Kleinert <info@manuelkleinert.ch>
2020-08-26 09:26:43 +0200

    update db

--- mount.sh
+++ mount.sh
@@ -4,10 +4,12 @@ docker-compose up -d
 docker-compose exec php chmod +x /var/www/html/bin/console
 docker-compose exec php composer install --prefer-dist
 docker-compose exec php chown -R www-data: /var/www/html
-docker-compose exec php php vendor/bin/pimcore-install --ignore-existing-config --admin-username admin --admin-password admin --mysql-username pimcore --mysql-database pimcore --mysql-host-socket db --mysql-password pimcore --mysql-port 3306 --no-interaction
+docker-compose exec php php vendor/bin/pimcore-install --ignore-existing-config --admin-username admin --admin-password admin --mysql-username pimcore --mysql-database pimcore --mysql-host-socket db --mysql-password pimcore --no-interaction
 docker-compose exec php php bin/console pimcore:bundle:enable WvisionBundle
 docker-compose down
 docker-compose up -d
 chmod 600 ./.deployer/id_deployer
 yarn install
 yarn dev
+
+mysql -u root -p -e "CREATE DATABASE pimcore charset=utf8mb4;"
